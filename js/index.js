c2e2194ebacae4221a4a85228574d3d5c7a5fe89
mianarsalan20 <mianarsalan20@gmail.com>
2021-09-25 03:17:18 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,63 @@
+//Top Sale owl carousel
+$(document).ready(function () {
+
+    // top sale owl carousel
+    $("#testimonials .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    
+    ///isoptops filter
+    var $grid = $(".grid").isotope({
+        itemSelector:'.grid-item',
+        layoutMode: 'fitRows'
+    }); 
+    
+    //filter itmes
+    $(".button-group").on("click","button",function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter:filterValue});
+        
+    })
+    
+    
+    // product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+
+    // click on qty up button
+    $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldval){
+                return ++oldval;
+            });
+        }
+    });
+
+       // click on qty down button
+       $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
+            });
+        }
+    });
+
+    
+    
+});
+
